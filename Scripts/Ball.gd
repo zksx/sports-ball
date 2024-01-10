25fa942a43d3ad66b9d38a8661c14d5774d53442
zksx <52799997+zksx@users.noreply.github.com>
2024-01-10 10:02:24 -0700

    improved ball physics, added some more vector math

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 func _ready():
-	set_velocity(Vector2(300, 300))
+	set_velocity(Vector2(200, 200))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var collison_info = move_and_collide(velocity * delta)
 	if collison_info:
+		var reflect = collison_info.get_remainder().bounce(collison_info.get_normal())
 		velocity = velocity.bounce(collison_info.get_normal())
+		move_and_collide(reflect)
