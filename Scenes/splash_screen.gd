29cd81427d7f2aa70044a8c07efbe6d636a026f4
zksx <52799997+zksx@users.noreply.github.com>
2024-01-31 11:48:49 -0700

    add some splash screen stuff

---
+++ Scenes/splash_screen.gd
@@ -0,0 +1,12 @@
+extends TextureRect
+var main_menu = preload("res://Scenes/main_menu.tscn")
+
+@onready var timer = $Timer
+
+func _ready():
+	pass
+
+func _process(_delta):
+	if (timer.is_stopped()):
+		get_tree().change_scene_to_packed(main_menu)
+
