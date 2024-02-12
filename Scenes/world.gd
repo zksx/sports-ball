cd0ac67f48a3cc93288cc1928aa049957e9c04c3
zksx <52799997+zksx@users.noreply.github.com>
2024-02-12 13:02:24 -0700

    made sample game manager scene and changed the world node to control the game instead of the court node

---
+++ Scenes/world.gd
@@ -0,0 +1,54 @@
+extends Node
+
+var gameManager = preload("res://Scripts/GameManager.tres")
+
+var p1_score = 0
+var p2_score = 0
+
+var serve_left = -100
+var serve_right = 100
+
+@export var Ball : PackedScene
+@onready var Server = $Server
+
+@onready var anim_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gameManager.say_hi()
+	ball_spawn()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	$player2_score.text = str(p1_score)
+	$player1_score.text = str(p2_score)
+
+func _on_player_1_goal_body_entered(body):
+	if body.is_in_group("disc"):
+
+		body.queue_free()
+		p2_score += 1
+		anim_player.play("point_reset")
+		Server.serve()
+
+func _on_player_2_goal_body_entered(body):
+	
+	if body.is_in_group("disc"):
+
+		body.queue_free()
+
+		p1_score += 1
+		anim_player.play("point_reset")
+		Server.serve()
+
+func victory():
+	if( p1_score >= 10):
+		print("p1 wins")
+	elif (p2_score >= 10):
+		print("p2 wins")
+
+func ball_spawn():
+	var ball = Ball.instantiate()
+	add_child(ball)
+	ball.serve()
+
---
+++ Scripts/GameManager.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name GameManager
+
+func say_hi():
+	print("I'm the Game Manager")
