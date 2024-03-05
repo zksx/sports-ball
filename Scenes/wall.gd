9467b0b479c7d52620fb8ef18f00c85ecf826160
zksx <52799997+zksx@users.noreply.github.com>
2024-03-05 14:09:57 -0700

    added bumpers and goals

--- Scenes/wall.gd
+++ Scenes/wall.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 
 @onready var audio_player = $"../AudioStreamPlayer2D"
-
+@onready var anim_player = $AnimationPlayer
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("disc"):
 		audio_player.play()
+		anim_player.play("hit")
--- Scripts/human_controller.gd
+++ Scripts/human_controller.gd
@@ -10,4 +10,4 @@ func _physics_process(_delta) -> void:
 	var movement_x_input = Input.get_action_strength(Controls.move_right) - Input.get_action_strength(Controls.move_left) 
 	var movement_y_input = Input.get_action_strength(Controls.move_down) - Input.get_action_strength(Controls.move_up)
 	var move_data = MovementCommand.Params.new(movement_x_input, movement_y_input )
-	movement_command.execute(player, move_data )
+	movement_command.execute(player, move_data)
